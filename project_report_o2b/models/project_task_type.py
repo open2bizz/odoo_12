ae43fdb1d9ce5d328fa985ee6aba822b3ad408b5
Thomas Pot <thomaspot@users.noreply.github.com>
2020-02-23 12:57:21 +0100

    Update project_task_type.py

--- project_report_o2b/models/project_task_type.py
+++ project_report_o2b/models/project_task_type.py
@@ -26,3 +26,14 @@ class SaleOrderText(models.Model):
 
     responsiblity_of_partner = fields.Boolean(string="Responsibility of Partner", help="When task is in this stage its the responsibility of the partner assigned to perform actions regarding this task")
     backlog_stage = fields.Boolean(string="Backlog stage", help="When task is in this stage it's treated as a backlog item, there is no funding for this item therefor it cannot be cannot be carried out yet.")
+
+class BacklogProject(models.Model):
+    _inherit = 'project.project'
+
+    backlog_project_id = fields.Many2one(
+        comodel_name = 'project.project',
+        string = 'Project Backlog',
+        required = False,
+        domain = "[('partner_id.commercial_partner_id', '=', partner_id.commercial_partner_id)]",
+        context = "{'default_partner_id' : partner_id,'default_allow_timesheets': False,'default_allow_forecast': False,'default_name': 'Backlog ' + name,'default_user_id': user_id}"
+    )    
